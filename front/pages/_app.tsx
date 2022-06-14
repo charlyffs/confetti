05d3ffbea18a184d156757de8cee19d6ad81f04c
Carlos M <carlosedm@protonmail.com>
2022-06-13 23:42:34 -0600

    Added TailwindCSS and MaterialUI

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
+import '../styles/tailwind.css'
 import type { AppProps } from 'next/app'
+import CssBaseline from '@mui/material/CssBaseline'
+import {StyledEngineProvider} from '@mui/material/styles'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyledEngineProvider injectFirst>
+      <CssBaseline />
+      <div className="absolute inset-0 bg-slate-900">
+
+      <Component {...pageProps} />
+      </div>
+    </StyledEngineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
